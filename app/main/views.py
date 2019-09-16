e4d621856567806f8af7224596c653f696104aef
marsvet <marsvet@163.com>
2019-09-17 00:09:35 +0800

    完成按起降地和时间查航班信息的接口

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template, request, jsonify
 from flask_login import login_required
 from . import main
 from ..models import Res
+import requests as req
+import json
 
 
 @main.route("/index")
@@ -21,3 +23,33 @@ def get_res_info():
         return jsonify(to_return)
     else:
         return jsonify({'isSuccess': True, '携带方式': '可以随身携带'})
+
+
+@main.route("/all_location")
+def get_all_location():
+    with open("app/static/data/location_code_mapping.json", 'r') as f:
+        mapping = json.loads(f.read())
+    return jsonify(list(mapping.keys()))
+
+@main.route("/flight_info_with_location", methods=["POST"])
+def get_flight_info_with_location():
+    with open("app/static/data/location_code_mapping.json", 'r') as f:
+        mapping = json.loads(f.read())
+
+    leave = request.json['leave']
+    arrive = request.json['arrive']
+    date = request.json['date']
+
+    headers = {"Authorization": "APPCODE 797a45df4c3e4876ad887686d674010f"}
+    data = {
+        "arrive_code": mapping[arrive],
+        "leave_code": mapping[leave],
+        "query_date": date
+    }
+
+    rsp = req.post("http://airinfo.market.alicloudapi.com/airInfos",
+                   data=data, headers=headers)
+    rsp.encoding = rsp.apparent_encoding
+    flightInfos = rsp.text
+
+    return jsonify(flightInfos)

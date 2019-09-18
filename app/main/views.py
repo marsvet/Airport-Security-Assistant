1523ed09f95f6c14ea222aa5e8ac66ad6c71b771
marsvet <marsvet@163.com>
2019-09-18 17:31:58 +0800

    完成按航班号查询航班信息的接口

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, jsonify
 from flask_login import login_required
+from lxml import etree
 from . import main
 from ..models import Res, City_code
 import requests as req
@@ -29,7 +30,8 @@ def get_all_location():
     city_code = City_code()
     return jsonify(city_code.get_all_city())
 
-@main.route("/flight_info_with_location", methods=["POST"])
+
+@main.route("/flight_info_by_location", methods=["POST"])
 def get_flight_info_with_location():
     city_code = City_code()
 
@@ -50,3 +52,39 @@ def get_flight_info_with_location():
     flightInfos = rsp.text
 
     return jsonify(flightInfos)
+
+
+@main.route("/flight_info_by_no", methods=["POST"])
+def get_flight_info_by_no():
+    flight_no = request.json['flightNo']
+    date = request.json['date']
+
+    url = "https://www.variflight.com/flight/fnum/%s.html?AE71649A58c77&fdate=%s" % (
+        flight_no, date.replace('-', ''))
+    headers = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+    rsp = req.get(url, headers=headers)
+
+    html = etree.HTML(rsp.content)
+    try:
+        row = html.xpath('//*[@id="list"]/li/div')[0]
+    except:
+        return jsonify({'isSuccess': False, 'msg': '查无此航班'})
+    try:
+        leave_location = row.xpath('span[4]/text()')[0]
+        arrive_location = row.xpath('span[7]/text()')[0]
+        flight_info = {
+            'isSuccess': True,
+            'flightNo' : row.xpath('span[1]/b/a[2]/text()')[0],
+            'airlineCompany': row.xpath('span[1]/b/a[1]/text()')[0],
+            'tkTime': row.xpath('span[2]/text()')[0].strip(),
+            'leavePort': leave_location[:-2] + "机场",
+            'leaveBuilding': leave_location[-2:] + "航站楼",
+            'arTime': row.xpath('span[5]/text()')[0].strip(),
+            'arrivePort': arrive_location[:-2] + "机场",
+            'arriveBuilding': arrive_location[-2:] + "航站楼",
+            'status': row.xpath('span[9]/text()')[0]
+        }
+        return jsonify(flight_info)
+    except:
+        return jsonify({'isSuccess': False, 'msg': "系统内部错误"})
